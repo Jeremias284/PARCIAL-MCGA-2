ae42d2daa64b74d7b901a76ea6ba7e4fd098ef71
ivan kopech <ivankopech@gmail.com>
2021-11-23 14:03:18 -0300

    PARCIAL MCGA: agrego rutas

--- src/Routers/Routes.jsx
+++ src/Routers/Routes.jsx
@@ -4,6 +4,7 @@ import Layout from '../components/Layout';
 import Home from '../components/Home';
 import Patients from '../components/Patients';
 
+
 const Routes = () => {
   return (
     <Switch>
