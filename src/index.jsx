b1ff8372597b00542406a8cde4b31fe9db385a35
ivan kopech <ivankopech@gmail.com>
2021-11-23 13:19:20 -0300

    PARCIAL MCGA: completo app.jsx e index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './redux/reducers/rootReducer';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const configureStore = () => {
+  const enhancer = composeWithDevTools(applyMiddleware(thunk));
+  return createStore(rootReducer, enhancer);
+};
+
+const store = configureStore();
+
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
