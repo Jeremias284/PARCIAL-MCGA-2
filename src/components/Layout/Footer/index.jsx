5bd73911b0b9d40d59cb2eb1bd91cf9f6682fb0c
ivan kopech <ivankopech@gmail.com>
2021-11-24 12:27:40 -0300

    PARCIAL MCGA: cambios en la vista de la pagina

--- src/components/Layout/Footer/index.jsx
+++ src/components/Layout/Footer/index.jsx
@@ -4,7 +4,7 @@ import styles from './footer.module.css';
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <p className={styles.text}>Ivan Kopech - Jeremias Calzada - 25-11-2021 </p>
+      <p className={styles.text}>Ivan Kopech - Jeremias Calzada - 25/11/2021 </p>
     </div>
   );
 };
