ea248513e70b63d752d81fbce85858111fbac2ea
Jeremias Calzada <calzadajeremias246@gmail.com>
2021-12-21 17:38:13 -0300

    FINAL MCGA: Eliminamos codigo ignesesario del App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './Routers/Routes';
-import {getPatients} from './Redux/Actions/patientActions';
-import { useDispatch } from 'react-redux';
-import {useEffect} from 'react';
+
 
 
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getPatients());
-    return () => {};
-  }, []);
-  
   return (
     <div>
       <Router>
