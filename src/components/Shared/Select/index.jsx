b0c47b27c0159f35c84ae1ecf05538577d086664
Jeremias Calzada <calzadajeremias246@gmail.com>
2021-12-21 14:49:10 -0300

    FINAL MCGA: Realizamos cambios en la carpeta Shared

--- src/components/Shared/Select/index.jsx
+++ src/components/Shared/Select/index.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import NativeSelect from '@mui/material/NativeSelect';
 import styles from './select.module.css';
 
 const Select = ({ input, meta, label, options }) => {
   return (
     <div className={styles.selectContainer}>
       <label className={styles.lbl}>{label}</label>
-      <select className={styles.sel} {...input}>
+      {/* <select className={styles.sel} {...input}> */}
+      <NativeSelect className={styles.sel} {...input}>
+
         {options.map((option) => (
           <option key={option.id} value={option.id}>
             {option.value}
           </option>
         ))}
-      </select>
+      {/* </select> */}
+      </NativeSelect>
       {meta.error && meta.touched && (
         <div className={styles.error}>{meta.error}</div>
       )}
--- src/components/Shared/TextInput/index.jsx
+++ src/components/Shared/TextInput/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import TextField from '@mui/material/TextField';
 import styles from './textInput.module.css';
 
-const TextInput = ({ input, label, placeholder, meta }) => {
+// const TextInput = ({ input, label, placeholder, meta }) => {
+  const TextInput = ({ input, label, placeholder, meta, size, variant }) => {
   return (
     <div className={styles.inputContainer}>
       <label className={styles.lbl}>{label}</label>
-      <input {...input} placeholder={placeholder} />
+      {/* <input {...input} placeholder={placeholder} /> */}
+      <TextField
+        {...input}
+        size={size}
+        variante={variant}
+        placeholder={placeholder}
+      />
       {meta.error && meta.touched && (
         <div className={styles.error}>{meta.error}</div>
       )}
